ab956c2c6c33f93bf40ec5de54553b017d905d9e
Pace Wasden <pacewasden@gmail.com>
2021-03-17 21:42:10 -0600

    Saving for the night

--- myapp/src/componets/App.js
+++ myapp/src/componets/App.js
@@ -1,5 +1,6 @@
 import logo from '../pictures/logo.svg';
 import '../style/App.css';
+import Home from './Home.js';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          <Home/>
+        </p>
       </header>
     </div>
   );
---
+++ myapp/src/componets/Home.js
@@ -0,0 +1,11 @@
+function Home(){
+    return (
+        <div>
+            <p>
+                This is going to be a blog to show what I learn through the nights while studying
+            </p>
+        </div>
+
+    )
+}
+export default Home;
