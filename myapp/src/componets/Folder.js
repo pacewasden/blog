23a2e5fbb5d979e98218bbc5208ba177405a0ec2
Pace Wasden <pacewasden@gmail.com>
2021-03-27 11:39:32 -0600

    Folder sitting over background image

--- myapp/src/componets/Folder.js
+++ myapp/src/componets/Folder.js
@@ -1,8 +1,8 @@
-
+import '../style/Folder.css';
+import folder from '../pictures/folder.png';
 function Folder(){
     return (
-
-
+        <img src={folder} className='folder' alt='folder'/>
     )
 }
 export default Folder;
--- myapp/src/componets/Home.js
+++ myapp/src/componets/Home.js
@@ -1,13 +1,10 @@
-// import background from '../pictures/background.png';
-// import folder from '../pictures/folder.png';
-import '../style/Folder.css';
+import Folder from './Folder.js'
+// import '../style/Folder.css';
 import '../style/Home.css';
 function Home(){
     return (
         <div className='homePage'>
-            <p>This is the website</p>
-            {/* <img src={background} className='background' alt='background'/> */}
-            {/* <img src={folder} className='folder' alt='folder'/> */}
+            <Folder/>
         </div>
 
     )
